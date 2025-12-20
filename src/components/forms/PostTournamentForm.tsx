e6d53bddd2292490536f4225702b220fbcfcfc45
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-20 15:25:05 +0530

    [dyad] Fixed TypeScript compile-time error in PostTournamentForm - wrote 1 file(s)

--- src/components/forms/PostTournamentForm.tsx
+++ src/components/forms/PostTournamentForm.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookhookform/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
