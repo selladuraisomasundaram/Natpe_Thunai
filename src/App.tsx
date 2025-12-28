15181301e8efbc6532d7f296a687df67208b45d9
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-28 20:14:53 +0530

    [dyad] Fixing TypeScript compile-time error and completing App.tsx - wrote 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
-import NotFound from "./pages/NotFound";
-import AuthPage from "./pages/AuthPage";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthPage from './pages/AuthPage';
+import HomePage from './pages/HomePage';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 export default App;
-// ... rest of your code
