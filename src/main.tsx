324bfbce1cfae5850ebbd832870a26a51593eda4
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-30 18:45:58 +0530

    [dyad] Fixing React Router context error - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./globals.css";
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
