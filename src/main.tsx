a42ac1d371ec9b683bdf823fea5822d92ac6368a
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-30 18:49:02 +0530

    [dyad] Ensuring single BrowserRouter in main.tsx - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; // Re-adding BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* Wrapping App with BrowserRouter */}
+    <BrowserRouter> {/* BrowserRouter should wrap the App component here */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
