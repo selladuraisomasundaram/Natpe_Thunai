15201b1d90e07e3155bf05bda855aef35dc99b83
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-30 18:49:30 +0530

    [dyad] Removing duplicate BrowserRouter from App.tsx - wrote 5 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter> {/* BrowserRouter should wrap the App component here */}
+    <BrowserRouter> {/* This is the ONLY place BrowserRouter should be */}
       <App />
     </BrowserRouter>
   </React.StrictMode>,
