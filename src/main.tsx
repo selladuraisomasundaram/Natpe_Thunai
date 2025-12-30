e4591a7503e251ae38758ef099d0680ec7cd0ae0
[dyad] <selladuraisomasundaram@gmail.com>
2025-12-30 18:47:28 +0530

    [dyad] Re-adding BrowserRouter to main.tsx - wrote 1 file(s) + extra files edited outside of Dyad

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-// Removed BrowserRouter from here, as it should be in App.tsx or a higher-level routing component.
+import { BrowserRouter } from 'react-router-dom'; // Re-adding BrowserRouter
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* Wrapping App with BrowserRouter */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
